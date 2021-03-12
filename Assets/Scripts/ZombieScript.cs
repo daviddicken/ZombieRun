e51f07b82eec752fb19512c5524b60dd90273781
daviddicken <davidfromseattle@gmail.com>
2021-03-11 23:24:50 -0800

    added zombies

---
+++ Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    
+    public GameObject zombie1;   
+    public GameObject zombie2;  
+    public GameObject zombie3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    public float timeRemaining = 1;
+    
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else 
+        {
+            float rand = Random.Range(1, 3);
+            Debug.Log("random number: " + rand);
+            GameObject randomZombie = new GameObject();
+            switch (rand) 
+            {
+                case 1:
+                    randomZombie = zombie1;
+                    break;
+                case 2:
+                    randomZombie = zombie2;
+                    break;
+                case 3:
+                    randomZombie = zombie3;
+                    break;
+            }
+            float pointX = Random.Range(-100, 100);
+            float pointZ = Random.Range(10, 190);
+
+            Instantiate(randomZombie, new Vector3(pointX, -1, pointZ), Quaternion.identity);
+            timeRemaining = 1; 
+        }
+
+        
+
+    }
+    
+  
+ 
+}
---
+++ Assets/ZombieCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieCollision : MonoBehaviour
+{
+   
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name != "Ground")
+        {
+            Debug.Log(collision.gameObject.name);
+            Destroy(gameObject);
+            Debug.Log("destroyed");
+
+        }
+
+        //Destroy(gameObject);
+    }
+}
